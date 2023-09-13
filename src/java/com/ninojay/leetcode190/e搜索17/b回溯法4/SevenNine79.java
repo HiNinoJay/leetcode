547921e8783522ac10cf1f60bdbbc00392c24549
nino <ninoJay@foxmail.com>
2023-09-13 12:42:56 +0800

    feature: 搜索79

--- src/java/com/ninojay/leetcode190/e搜索17/b回溯法4/SevenNine79.java
+++ src/java/com/ninojay/leetcode190/e搜索17/b回溯法4/SevenNine79.java
@@ -8,53 +8,63 @@ import com.ninojay.leetcode190.util.NinoArrayUtil;
  */
 public class SevenNine79 {
 
-    static boolean result = false;
 
     public static void main(String[] args) {
         char[][] board = NinoArrayUtil.createTwoDimensionCharArray("[" +
                 "[\"A\",\"B\",\"C\",\"E\"]," +
                 "[\"S\",\"F\",\"C\",\"S\"]," +
                 "[\"A\",\"D\",\"E\",\"E\"]]");
-        String word = "ABCB";
-        System.out.println(exist(board, word));
+        String word = "ABC";
+        System.out.println(new SevenNine79().exist(board, word));
     }
 
-    public static boolean exist(char[][] board, String word) {
+    private int[] position = new int[]{-1, 0, 1, 0, -1};
+    private boolean result = false;
+
+    public boolean exist(char[][] board, String word) {
+
+        int currentIndex = 0;
+        char[] words = word.toCharArray();
+
         int rows = board.length;
         int columns = board[0].length;
-
         boolean[][] visited = new boolean[rows][columns];
 
         for(int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                 dfs(board, i, j, word, 0, visited);
+            for(int j = 0; j < columns; j++) {
+                if(board[i][j] == words[0]) {
+                    dfs(visited, board, i, j, words, currentIndex);
+                }
             }
         }
         return result;
-
     }
 
-    private static void dfs(char[][] board, int row, int column, String word, int postion, boolean[][] visited) {
+    private void dfs(boolean[][] visited, char[][] board, int i, int j, char[] words, int currentIndex) {
 
-        // 遇到边界
-        if(row < 0 || row >= board.length || column < 0 || column >= board[0].length) {
+        if(currentIndex == words.length - 1) {
+            if(board[i][j] == words[currentIndex]) {
+                result = true;
+            }
+        }
+
+        if(board[i][j] != words[currentIndex]) {
             return;
         }
-
-        if(!visited[row][column] && board[row][column] == word.charAt(postion)) {
-            visited[row][column] = true;
-
-            if(postion == word.length() - 1)  {
-                result = true;
-                return;
+        visited[i][j] = true;
+        // 来到这里就说明当前字符相同，找下一个相同字符
+        for(int k = 0; k < 4; k++) {
+            int m = i + position[k];
+            int n = j + position[k+1];
+            if(m >= 0 && m < board.length && n >=0 && n < board[0].length
+                    && !visited[m][n]
+                    && (currentIndex + 1 < words.length) && words[currentIndex + 1] == board[m][n]) {
+                dfs(visited, board, m, n, words, currentIndex + 1);
             }
-            dfs(board, row - 1 , column, word, postion + 1, visited);
-            dfs(board, row + 1, column, word, postion + 1, visited);
-            dfs(board, row , column - 1, word, postion + 1, visited);
-            dfs(board, row , column + 1, word, postion + 1, visited);
-            visited[row][column] = false;
         }
-
+        visited[i][j] = false;
     }
+
+
 
 }
