098fb762a9e26706b3835f247683abd30d27901f
nino <ninoJay@foxmail.com>
2023-09-16 01:02:28 +0800

    feature: 搜索47

--- src/java/com/ninojay/leetcode190/e搜索16/FourSeven47.java
+++ src/java/com/ninojay/leetcode190/e搜索16/FourSeven47.java
@@ -13,46 +13,60 @@ import java.util.List;
  */
 public class FourSeven47 {
 
-    private static HashSet<String> demo = new HashSet<>();
-    private static List<List<Integer>> result = new ArrayList<>();
 
     public static void main(String[] args) {
-        int[] nums = NinoArrayUtil.createOneDimensionArray(1, 2, 3);
-        NinoCollectionUtil.printListListInteger(permuteUnique(nums));
+        int[] nums = NinoArrayUtil.createOneDimensionArray(1, 1, 2);
+        NinoCollectionUtil.printListListInteger(new FourSeven47().permuteUnique(nums));
     }
 
-    public static List<List<Integer>> permuteUnique(int[] nums) {
-        backTracking(nums, 0);
-        return result;
+
+    private List<List<Integer>> result = new ArrayList<>();
+
+    private HashSet<String> theOne = new HashSet<>();
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+
+        if(nums == null) {
+            return result;
+        }
+        backTracking(nums,  0);
+        return  result;
     }
 
-    private static void backTracking(int[] nums, int index) {
+    private void backTracking(int[] nums, int index) {
+
+        if(index >= nums.length) {
+            return;
+        }
 
 
         if(index == nums.length - 1) {
-            StringBuilder sb = new StringBuilder();
-            ArrayList<Integer> temp_result = new ArrayList<>();
-            for(int i = 0; i < nums.length; i++) {
-                sb.append(nums[i]);
-                temp_result.add(nums[i]);
+            List<Integer> tempResult = new ArrayList<>();
+            StringBuilder tempOne = new StringBuilder();
+            for(Integer num : nums) {
+                tempResult.add(num);
+                tempOne.append(num + ",");
             }
-            if(!demo.contains(sb.toString())) {
-                demo.add(sb.toString());
-                result.add(temp_result);
+            if(!theOne.contains(tempOne.toString())) {
+                theOne.add(tempOne.toString());
+                result.add(tempResult);
             }
             return;
         }
+
         for(int i = index; i < nums.length; i++) {
             swap(nums, i, index);
             backTracking(nums, index + 1);
             swap(nums, i, index);
         }
+
     }
 
-    private static void swap(int[] nums, int a, int b) {
+    private void swap(int[] nums, int a, int b) {
         int temp = nums[a];
         nums[a] = nums[b];
         nums[b] = temp;
     }
+
 
 }
